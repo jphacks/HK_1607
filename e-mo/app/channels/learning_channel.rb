162e47436dbd5f319be9727395f6071e8f28215f
jdbtisk <de7aef3fd77mt6@gmail.com>
2016-11-06 06:54:27 +0900

    job, channelにコメント追加

--- e-mo/app/channels/learning_channel.rb
+++ e-mo/app/channels/learning_channel.rb
@@ -1,15 +1,14 @@
-# Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class LearningChannel < ApplicationCable::Channel
 
   # Connection.connetの後に呼ばれる
   def subscribed
     stream_from "learning_channel"
     stream_for current_user
+    # カレントユーザが先生でない場合(生徒だった場合)接続数を先生に通知
     ConnectCountJob.perform_later if current_user.teacher_flag
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
     # 切断したユーザが先生ではない場合(生徒だった場合)
     if !self.current_user.teacher_flag
       # ユーザの接続フラグをオフに
@@ -20,8 +19,9 @@ class LearningChannel < ApplicationCable::Channel
   end
 
   def speak(data)
-    # 先生宛てにメッセージ送信
+    # 登録されている先生全てに対する処理
     User.where(teacher_flag: true).each do |teacher|
+      # メッセージ送信
       LearningChannel.broadcast_to(teacher, {
         student_name: current_user.student_name, message: data["message"], time: Time.now.strftime("%Y年%m月%d日 %H時%M分%S秒")
       })
--- e-mo/app/jobs/connect_count_job.rb
+++ e-mo/app/jobs/connect_count_job.rb
@@ -1,12 +1,14 @@
 class ConnectCountJob < ApplicationJob
   queue_as :default
 
+  # 先生宛てで生徒の接続数を通知
   def perform
-    # 先生宛てで生徒の接続数を通知
+    # 登録されている先生全てに対する処理
     User.where(teacher_flag: true).each do |teacher|
+      # 接続数を通知
       LearningChannel.broadcast_to(teacher, {
         connected_count: User.where(connected_flag: true).count
-        })
+      })
     end
   end
 end
