008329652dc37a22747f7377f73f2ea01ae8dd59
iwaseatsuya <aaa.1470.333@gmail.com>
2016-11-05 15:00:46 +0900

    productionの設定

--- e-mo/config/environments/production.rb
+++ e-mo/config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -37,7 +37,10 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://desolate-basin-88713.herokuapp.com', 'http://desolate-basin-88713.herokuapp.com' ]
+  # Not Default
+  config.web_socket_server_url = "wss://desolate-basin-88713.herokuapp.com/cable"
+
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
