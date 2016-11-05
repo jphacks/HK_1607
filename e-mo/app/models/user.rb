70606582f0f87043a9457e7ab1b90333c7a3cc37
iwaseatsuya <aaa.1470.333@gmail.com>
2016-11-05 17:56:57 +0900

    ユーザモデルに制約の追加と先生画面のhtml記述

--- e-mo/app/models/user.rb
+++ e-mo/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :login_id, presence: true, length: { maximum: 20 }
+  validates :password, presence: true, length: { maximum: 255 }
+  validates :student_name, length: { maximum: 40 }
 end
