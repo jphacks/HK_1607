d613aeafbe442ee1d52e000de476049356dfed22
iwaseatsuya <aaa.1470.333@gmail.com>
2016-11-05 16:08:03 +0900

    チャット実装

--- e-mo/app/channels/learning_channel.rb
+++ e-mo/app/channels/learning_channel.rb
@@ -1,13 +1,15 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class LearningChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "learning_channel"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def speak
+  def speak(data)
+    ActionCable.server.broadcast "learning_channel",
+        { student_name: data["name"], message: data["message"], time: Time.now.strftime("%Y年%m月%d日 %H時%M分%S秒") }
   end
 end
