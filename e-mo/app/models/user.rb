ca365d8cba5d8eb2faf3e6a27b3adf3eaf52268b
jdbtisk <de7aef3fd77mt6@gmail.com>
2016-11-06 06:59:23 +0900

    userモデルに制約追加

--- e-mo/app/models/user.rb
+++ e-mo/app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-  validates :login_id, presence: true, length: { maximum: 20 }
-  validates :password, presence: true, length: { maximum: 255 }
+  # ログインIDの入力必須化, 最大文字数及び最小文字数の設定
+  validates :login_id, presence: true, length: { maximum: 20, minimum: 4 }
+  # パスワードの入力必須化, 最大文字数及び最小文字数の設定
+  validates :password, presence: true, length: { maximum: 255, minimum: 8 }
+  # 生徒名の最大文字数の設定
   validates :student_name, length: { maximum: 40 }
 end
